a01a36287598add1cb78c2c724c962050e7a7925
khkwon01 <khkwon01@gmail.com>
2024-11-27 14:07:39 +0900

    Create dump.js

---
+++ mig/dump.js
@@ -0,0 +1,27 @@
+const session = mysql.getSession("admin:Welcome#1@10.1.1.12");
+
+// Set the session as the active global session
+shell.setSession(session);
+
+// Perform a schema dump with  options
+try {
+    // Perform the schema dump
+    print("Starting dump of schema(s): qb");
+    util.dumpSchemas(['qb'], './qbdump', {
+        threads: 2,
+	showProgress: true,
+        ocimds: true, // Enable OCI MDS metadata
+	//consistent: false,  // consistent lock disable
+	compatibility: ["strip_definers", "strip_restricted_grants", "ignore_missing_pks"]
+    });
+    
+    // If successful, print a confirmation message
+    print("Dump completed successfully.");
+} catch (err) {
+    // If an error occurs, print the error and exit the script
+    print("\nError during dump: " + err.message);
+    shell.exit(1)
+}
+
+// Close the session when done
+session.close();

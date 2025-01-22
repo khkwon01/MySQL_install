acc1c7373854c4b932dd9ceeae5b29d72e4c9065
khkwon01 <khkwon01@gmail.com>
2025-01-22 14:45:06 +0900

    Update mysql_install_8.0.41.sh

--- mysql_install_8.0.41.sh
+++ mysql_install_8.0.41.sh
@@ -31,8 +31,8 @@ fi
 export log_file="${working_dir}/$(basename -s .sh $0).log"
 export sw_dir="${working_dir}/pkg"
 
-export MOS_LINK_SRV_TAR='https://updates.oracle.com/Orion/Services/download/p37167423_580_Linux-x86-64.zip?aru=25874142&patch_file=p37167423_580_Linux-x86-64.zip'
-export MOS_LINK_SHELL_TAR='https://updates.oracle.com/Orion/Services/download/p37172762_800_Linux-x86-64.zip?aru=25877020&patch_file=p37172762_800_Linux-x86-64.zip'
+export MOS_LINK_SRV_TAR='https://updates.oracle.com/Orion/Services/download/p37498809_580_Linux-x86-64.zip?aru=26042021&patch_file=p37498809_580_Linux-x86-64.zip'
+export MOS_LINK_SHELL_TAR='https://updates.oracle.com/Orion/Services/download/p37499669_800_Linux-x86-64.zip?aru=26042605&patch_file=p37499669_800_Linux-x86-64.zip'
 
 export AIRPORT_DB='https://downloads.mysql.com/docs/airport-db.tar.gz'
 
